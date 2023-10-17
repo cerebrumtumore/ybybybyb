f8d004dca1c9c4fb08e7a00008ad79594c20f71a
cerebrumtumore <147840738+cerebrumtumore@users.noreply.github.com>
2023-10-17 14:04:29 +0500

    Add files via upload

---
+++ гг.js
@@ -0,0 +1,23 @@
+var num = 5
+
+if (num==1) {
+    console.log("Пон")
+}
+else if (num==2) {
+    console.log("Вторник") 
+}
+else if (num==3) {
+    console.log("Среда") 
+}
+else if (num==4) {
+    console.log("Четверг")
+}
+else if (num==5) {
+    console.log("Пятница")  
+}
+else if (num==6) {
+    console.log("Суббота") 
+}
+else if (num==7) {
+    console.log("Воскресенье")
+}           
